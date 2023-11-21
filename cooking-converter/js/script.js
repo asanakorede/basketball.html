1869889493c00508e429da6e9f8bc3d6e2e81f03
Asana Korede <asanakorededaniel@gmail.com>
2023-11-21 10:22:30 -0800

    add 6 conversion functions

---
+++ cooking-converter/js/script.js
@@ -0,0 +1,84 @@
+
+$(document).ready(function () {
+
+
+
+    function convertTbsptoCup(number1) {
+        let number2 = 1 / 8 * number1
+        return number2;
+    }
+
+    function convertCuptoTbsp(number2) {
+        let number1 = 8 * number2
+        return number1;
+    }
+
+
+    function convertTsptoTbsp(number1) {
+        let number2 = number1 / 3
+        return number2;
+    }
+
+    function convertTbsptoTsp(number2) {
+        let number1 = 3 * number2
+        return number1;
+    }
+
+
+    function convertCuptoOunce(number1) {
+        let number2 = 8 * number1
+        return number2;
+    }
+
+    function convertOuncetoCup(number2){
+         let number1 = number2 / 8
+         return number1;
+    }
+
+
+
+    //  convertTbsptoCup(8)
+
+
+
+
+    // let number3 = parseInt(prompt('Number of Tablespoons:'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
